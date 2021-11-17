141f9bbf54f0054cb821fbcccf2648e36d162f29
Kyung Jae Kim <gesture02@gmail.com>
2021-11-17 22:04:06 +0900

    ec2

---
+++ post/src/main/java/post/Post.java
@@ -0,0 +1,40 @@
+package post;
+
+public class Post {
+	private int id;
+	private String title;
+	private String content;
+	public int getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", title=" + title + ", content=" + content + "]";
+	}
+	
+	public Post() {
+		
+	}
+
+	public Post(int id, String title, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
